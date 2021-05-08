0adf41e28fa2e3e5ee856ce1ef60115e7736fac3
wenbyin <wenbyin@tesla.com>
2021-05-08 14:32:59 +0800

    modify param from pointer

--- mysql/mysql.go
+++ mysql/mysql.go
@@ -15,8 +15,8 @@ type Driver struct {
 	oldDb *sql.DB
 }
 
-var DefaultStrWithoutQuote = []string{"CURRENT_TIMESTAMP"}
-var Extra = []string{"auto_increment"}
+var defaultStrWithoutQuote = []string{"CURRENT_TIMESTAMP"}
+var extra = []string{"auto_increment"}
 
 // New creates a new Driver driver.
 // The DSN is documented here: https://github.com/go-sql-driver/mysql#dsn-data-source-name
@@ -316,7 +316,7 @@ func (d *Driver) Generate(result *dbdiffer.Result) ([]string, error) {
 			sql := "CREATE TABLE `" + tablename + "` ("
 			fieldstr := make([]string, 0)
 			for _, field := range fields {
-				fieldstr = append(fieldstr, "`"+field.Field+"` "+strings.ToUpper(field.Type)+sqlnull(field.Null)+sqldefault(field.Default)+sqlextra(field.Extra)+sqlcomment(field.Comment))
+				fieldstr = append(fieldstr, "`"+field.Field+"` "+strings.ToUpper(field.Type)+sqlnull(field.Null)+sqldefault(*field.Default)+sqlextra(field.Extra)+sqlcomment(field.Comment))
 			}
 			if indexes, exists := result.Indexes.Create[tablename]; exists {
 				for _, index := range indexes {
@@ -369,7 +369,7 @@ func (d *Driver) Generate(result *dbdiffer.Result) ([]string, error) {
 	if len(result.Fields.Add) > 0 {
 		for tablename, fields := range result.Fields.Add {
 			for name, detail := range fields {
-				sqls = append(sqls, "ALTER TABLE `"+tablename+"` ADD `"+name+"` "+strings.ToUpper(detail.Type)+sqlcol(detail.Collation)+sqlnull(detail.Null)+sqldefault(detail.Default)+sqlextra(detail.Extra)+sqlcomment(detail.Comment)+" AFTER `"+detail.After+"`;")
+				sqls = append(sqls, "ALTER TABLE `"+tablename+"` ADD `"+name+"` "+strings.ToUpper(detail.Type)+sqlcol(detail.Collation)+sqlnull(detail.Null)+sqldefault(*detail.Default)+sqlextra(detail.Extra)+sqlcomment(detail.Comment)+" AFTER `"+detail.After+"`;")
 			}
 		}
 	}
@@ -387,7 +387,7 @@ func (d *Driver) Generate(result *dbdiffer.Result) ([]string, error) {
 	if len(result.Fields.Change) > 0 {
 		for tablename, fields := range result.Fields.Change {
 			for name, detail := range fields {
-				sqls = append(sqls, "ALTER TABLE `"+tablename+"` CHANGE `"+name+"` `"+name+"` "+strings.ToUpper(detail.Type)+sqlcol(detail.Collation)+sqlnull(detail.Null)+sqldefault(detail.Default)+sqlextra(detail.Extra)+sqlcomment(detail.Comment)+";")
+				sqls = append(sqls, "ALTER TABLE `"+tablename+"` CHANGE `"+name+"` `"+name+"` "+strings.ToUpper(detail.Type)+sqlcol(detail.Collation)+sqlnull(detail.Null)+sqldefault(*detail.Default)+sqlextra(detail.Extra)+sqlcomment(detail.Comment)+";")
 			}
 		}
 	}
@@ -554,19 +554,19 @@ func sqlnull(s string) string {
 	}
 }
 
-func sqldefault(s *string) string {
-	if s == nil {
+func sqldefault(s string) string {
+	if s == "" {
 		return ""
 	}
-	if val, exists := ExistInArray(s, DefaultStrWithoutQuote); exists {
+	if val, exists := ExistInArray(s, defaultStrWithoutQuote); exists {
 		return " DEFAULT " + val
 	}
-	return " DEFAULT '" + escape(*s) + "'"
+	return " DEFAULT '" + escape(s) + "'"
 }
 
-func ExistInArray(s *string, strArray[] string) (val string, exists bool) {
+func ExistInArray(s string, strArray[] string) (val string, exists bool) {
 	for _, obj := range strArray {
-		if obj == *s {
+		if strings.EqualFold(obj, s) {
 			return obj, true
 		}
 	}
@@ -575,7 +575,7 @@ func ExistInArray(s *string, strArray[] string) (val string, exists bool) {
 }
 
 func sqlextra(s string) string {
-	if val, exists := ExistInArray(&s, Extra); exists {
+	if val, exists := ExistInArray(s, extra); exists {
 		return " " + val
 	}
 	return ""
